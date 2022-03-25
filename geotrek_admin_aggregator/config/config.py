b57ed77614dfe4f1e7a89998c1d09836cd12fb43
idjepa <idrizza@hotmail.fr>
2022-03-25 15:31:26 +0100

    essais utilisation modèles Django

--- geotrek_admin_aggregator/config/config.py
+++ geotrek_admin_aggregator/config/config.py
@@ -1,7 +1,5 @@
 API_BASE_URL = 'https://geotrekdemo.ecrins-parcnational.fr/api/v2/' ## URL de l'API v2 de la base de données source
 AUTHENT_STRUCTURE = 'PNE' ## Nom de la source des données. Doit correspondre à une entrée dans la table "authent_structure". Indispensable pour la tracabilité des données.
-SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@host:port/dbname' ## URI de la base de données aggregator, au format SQLAlchemy
 GAG_BASE_LANGUAGE = 'fr' ## langue par défaut de la base de données aggregator
-SRID = 2154 ## SRID du système de coordonées de la base de données aggregator
+SRID = 2154 ## SRID du système de coordonnées de la base de données aggregator
 
-SQLALCHEMY_TRACK_MODIFICATIONS = False ## Désactive le suivi des modifications de la part de SQLAlchemy
---
+++ geotrek_admin_aggregator/custom_geotrek_shell.py
@@ -0,0 +1,24 @@
+import requests
+from geotrek.trekking.models import Trek, POIType
+from geotrek.common.geotrek_admin_aggregator.config.config import API_BASE_URL, GAG_BASE_LANGUAGE
+
+
+def fetch_api(API_BASE_URL):
+    url = API_BASE_URL + "poi_type/15"
+
+    print("Fetching API...")
+    response = requests.get(url)
+    r = response.json()#["results"]
+    print(r)
+
+    newPoiType = POIType.objects.create(label=r["label"][GAG_BASE_LANGUAGE])
+    newPoiType.save()
+
+
+
+def get_name_trek(identifiant):
+    trek = Trek.objects.get(id=identifiant)
+    trek_name = trek.name_fr
+    print(trek_name)
+
+fetch_api(API_BASE_URL)
