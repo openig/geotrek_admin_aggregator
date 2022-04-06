3ac952f90628ff077e17f9dc03aea8ac25bb8e76
idjepa <idrizza@hotmail.fr>
2022-04-06 17:16:45 +0200

    deleting from uuids fetching & date_update filter

--- gag_app/config/config.py
+++ gag_app/config/config.py
@@ -9,4 +9,4 @@ SRID = 2154 ## SRID du système de coordonnées de la base de données aggregato
 #https://geotrek-admin.cevennes-parcnational.net/api/v2/ PNC portal 'DEP_48'
 #https://admin48.openig.org/api/v2/ Conseil départemental de la Lozère portal None
 #https://geotrekdemo.ecrins-parcnational.fr/api/v2/ PNE portal None
-#https://openig-geotrek-pnrgca.ataraxie.fr/api/v2/ portal 1 ??
+#https://openig-geotrek-pnrgca.ataraxie.fr/api/v2/ portal 1 
