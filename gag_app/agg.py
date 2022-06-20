e7fd842dbbac1537a83cd1242b87656dae87d9d2
Idrissa Djepa Creutz <73114156+IdrissaD@users.noreply.github.com>
2022-06-20 13:54:00 +0200

    Continue aggregation of remaining sources if an exception occurs for one source - #42

--- gag_app/agg.py
+++ gag_app/agg.py
@@ -17,31 +17,34 @@ log.setLevel(logging.DEBUG)
 
 
 for source in SOURCES:
-    tic = perf_counter()
+    try:
+        tic = perf_counter()
 
-    # Encapsulate script in transaction to avoid import of partial data
-    with transaction.atomic():
-        coretopology_fields = Topology._meta.get_fields()
-        structure = Structure.objects.get(name=source['AUTHENT_STRUCTURE'])
-        api_base_url = f"https://{source['GADMIN_BASE_URL']}/api/v2/"
+        # Encapsulate script in transaction to avoid import of partial data
+        with transaction.atomic():
+            coretopology_fields = Topology._meta.get_fields()
+            structure = Structure.objects.get(name=source['AUTHENT_STRUCTURE'])
+            api_base_url = f"https://{source['GADMIN_BASE_URL']}/api/v2/"
 
-        log.info("Checking API version...")
-        version = requests.get(api_base_url + 'version').json()['version']
-        log.info(f'API version is: {version}')
+            log.info("Checking API version...")
+            version = requests.get(api_base_url + 'version').json()['version']
+            log.info(f'API version is: {version}')
 
-        # Iterate in env.model_to_import, which sets models processing order
-        for mti_name in source['DATA_TO_IMPORT']:
-            main_parser = ParserAPIv2ImportContentTypeModel(
-                api_base_url=api_base_url,
-                model_to_import_name=mti_name,
-                model_to_import_properties=model_to_import[mti_name],
-                structure=structure,
-                coretopology_fields=coretopology_fields,
-                AUTHENT_STRUCTURE=source['AUTHENT_STRUCTURE'],
-                IMPORT_ATTACHMENTS=source['IMPORT_ATTACHMENTS'],
-            )
+            # Iterate in env.model_to_import, which sets models processing order
+            for mti_name in source['DATA_TO_IMPORT']:
+                main_parser = ParserAPIv2ImportContentTypeModel(
+                    api_base_url=api_base_url,
+                    model_to_import_name=mti_name,
+                    model_to_import_properties=model_to_import[mti_name],
+                    structure=structure,
+                    coretopology_fields=coretopology_fields,
+                    AUTHENT_STRUCTURE=source['AUTHENT_STRUCTURE'],
+                    IMPORT_ATTACHMENTS=source['IMPORT_ATTACHMENTS'],
+                )
 
-            main_parser.delete_update_insert_data(PORTALS=source['PORTALS'])
+                main_parser.delete_update_insert_data(PORTALS=source['PORTALS'])
 
-    toc = perf_counter()
-    log.info(f'Performed aggregation in {toc - tic:.0f} seconds')
+        toc = perf_counter()
+        log.info(f'Performed aggregation in {toc - tic:.0f} seconds')
+    except:
+        log.error(f'{source} aggregation was stopped because an exception occurred.')
